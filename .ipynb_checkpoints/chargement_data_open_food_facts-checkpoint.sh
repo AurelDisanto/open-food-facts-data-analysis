b847b2ba80fbbd4b2da64324176baefdec519af3
auredisanto <aurelien.disanto@insee.fr>
2024-05-03 15:59:52 +0000

    ajout analyse csv

--- .ipynb_checkpoints/chargement_data_open_food_facts-checkpoint.sh
+++ .ipynb_checkpoints/chargement_data_open_food_facts-checkpoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-# curl -o openfoodfacts-products.jsonl.gz https://static.openfoodfacts.org/data/openfoodfacts-products.jsonl.gz
+# csv
+curl -o en.openfoodfacts.org.products.csv.gz https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz
 
+gzip -d en.openfoodfacts.org.products.csv.gz
+
+# dump mongodb
 curl -o openfoodfacts-mongodbdump.tar.gz https://static.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz
 
 tar -xf openfoodfacts-mongodbdump.tar.gz
 
 mongodb-database-tools-ubuntu2204-x86_64-100.9.4/bin/mongorestore $CONNECTION_STRING_MONGO_DB dump/off/products.bson
+
+# jsonl
+ curl -o openfoodfacts-products.jsonl.gz https://static.openfoodfacts.org/data/openfoodfacts-products.jsonl.gz
+
+gzip -d en.openfoodfacts.org.products.csv.gz
