c8ba206010ce70506091b902c6dc50247e48c467
auredisanto <aurelien.disanto@insee.fr>
2024-05-05 16:53:40 +0000

    analyse via le fichier jsonl OK

--- .ipynb_checkpoints/chargement_data_open_food_facts-checkpoint.sh
+++ .ipynb_checkpoints/chargement_data_open_food_facts-checkpoint.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-# csv
+# récupération du csv en local dans le conteneur => non retenu in fine
 curl -o en.openfoodfacts.org.products.csv.gz https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz
 
 gzip -d en.openfoodfacts.org.products.csv.gz
 
-# dump mongodb
+# récupération du dump mongodb en local dans le conteneur => non retenu in fine
 curl -o openfoodfacts-mongodbdump.tar.gz https://static.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz
 
 tar -xf openfoodfacts-mongodbdump.tar.gz
 
 mongodb-database-tools-ubuntu2204-x86_64-100.9.4/bin/mongorestore $CONNECTION_STRING_MONGO_DB dump/off/products.bson
 
-# jsonl
+# récupération du jsonl en local dans le conteneur => format retenu in fine
 curl -o openfoodfacts-products.jsonl.gz https://static.openfoodfacts.org/data/openfoodfacts-products.jsonl.gz
 
 gzip -d openfoodfacts-products.jsonl.gz
+
+# copie sur le s3 du datalab des fichiers csv et parquet contenant les données d'intérêt extraites à partir du jsonl
+
+mc cp "/home/onyxia/work/data_interet.csv" s3/auredisanto/open-food-facts-analysis/data_interet.csv
+
+mc cp "/home/onyxia/work/data_interet.parquet" s3/auredisanto/open-food-facts-analysis/data_interet.parquet
